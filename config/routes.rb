c22e6bf1f0741493de24ad0904227e9568dfe9a8
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-06-08 14:19:57 +0200

    added general pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Oldtimersv2::Application.routes.draw do
+  get "general_pages/home"
+
+  root :to => "general_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/helpers/general_pages_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class GeneralPagesHelperTest < ActionView::TestCase
+end
