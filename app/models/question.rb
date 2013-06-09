c8efef23eddaeb6b1afb1e6f9368948b4f348663
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-06-09 17:41:34 +0200

    added select2

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
 	has_and_belongs_to_many :categories
-
 	has_many :labels, :dependent => :destroy
 
+	accepts_nested_attributes_for :categories, :reject_if => lambda { |l| l[:title].blank? }, allow_destroy: true
 	accepts_nested_attributes_for :labels, :reject_if => lambda { |l| l[:title].blank? }, :allow_destroy => true
 end
