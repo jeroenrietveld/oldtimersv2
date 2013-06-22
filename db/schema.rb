a37e68d60d68e08d42622a0ce3979728d2c63b95
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-06-22 14:19:07 +0200

    make register button route to correct page and added new user fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130619210208) do
+ActiveRecord::Schema.define(version: 20130622121510) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -92,6 +92,9 @@ ActiveRecord::Schema.define(version: 20130619210208) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
